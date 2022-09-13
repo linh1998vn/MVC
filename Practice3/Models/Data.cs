6dc1e3e5a840a65415de2be75e410e2792309f9a
linh1998vn <81789760+linh1998vn@users.noreply.github.com>
2022-09-14 00:09:22 +0700

    Practice3

---
+++ Practice3/Models/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Practice3.Models
+{
+    public class Data: DbContext
+    {
+        public Data() : base("DBConnection")
+        {
+
+        }
+
+        public DbSet<Category> classes { get; set; }
+
+        public DbSet<Product> products { get; set; }
+    }
+}
